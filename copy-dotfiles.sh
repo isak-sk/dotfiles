4644405b5495af7e66dfe0d90e51b4d6bc5b5e12
Isak <skoog.isak@gmail.com>
2026-03-08 23:46:20 +0100

    add script, update files

---
+++ copy-dotfiles.sh
@@ -0,0 +1,5 @@
+cp ~/.config/starship/starship.toml cli-tools/starship.toml
+cp ~/.config/fastfetch/config.jsonc fastfetch/gruvbox.jsonc
+cp ~/.config/ghostty/config ghostty/config
+cp -r ~/.config/helix/ .
+cp ~/.zshrc shells/zshrc
